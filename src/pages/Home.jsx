60c320b92a1d7d707cc050fc1271c6b1592e8e28
User <adam.kaleta97@gmail.com>
2025-02-06 19:32:26 +0100

    Working on backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,90 @@
+import { useEffect, useState } from "react";
 import { Layout } from "../components/layout/Layout";
 import { PostView } from "../components/PostView";
 
-export const Home = () => (
-    <Layout>
-        <div className="main-content right-chat-active">
-            <div className="middle-sidebar-bottom">
-                <div className="middle-sidebar-left">
-                    <div className="row feed-body">
-                        <div className="col-xl-8 col-xxl-9 col-lg-8">
-                            <PostView user="Zbigniew" time="12:30" avatar="download1.png" />
-                            <PostView user="Adam" time="12:42" avatar="star.png" />
-                            <PostView user="Ewa" time="16:30" avatar="chip.png" />
-                            <PostView user="Józek" time="14:30" avatar="avatar.jpg" />
-                            <PostView user="Julia" time="2:30" avatar="avatar.jpg" />
+// const posts = [
+//     {
+//         user: "Zbigniew",
+//         time: "12:30",
+//         avatar: "download3.png",
+//         postimage: "post.png",
+//         description:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia illo enim hic quas consequuntur minus pariatur consectetur? Culpa minima voluptates maiores molestiae error, neque at tempora deserunt sint repellendus?",
+
+//     },
+//     {
+//         user: "Adam",
+//         time: "14:30",
+//         avatar: "star.png",
+//         postimage: "post.png",
+//         description:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia illo enim hic quas consequuntur minus pariatur consectetur? Culpa minima voluptates maiores molestiae error, neque at tempora deserunt sint repellendus?",
+
+//     },
+//     {
+//         user: "Ewa",
+//         time: "1:30",
+//         avatar: "chip.png",
+//         postimage: "post.png",
+//         description:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia illo enim hic quas consequuntur minus pariatur consectetur? Culpa minima voluptates maiores molestiae error, neque at tempora deserunt sint repellendus?",
+
+//     },
+//     {
+//         user: "Józek",
+//         time: "6:30",
+//         avatar: "download2.png",
+//         postimage: "post.png",
+//         description:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia illo enim hic quas consequuntur minus pariatur consectetur? Culpa minima voluptates maiores molestiae error, neque at tempora deserunt sint repellendus?",
+
+//     },
+//     {
+//         user: "Julia",
+//         time: "12:10",
+//         avatar: "download1.png",
+//         postimage: "post.png",
+//         description:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quia illo enim hic quas consequuntur minus pariatur consectetur? Culpa minima voluptates maiores molestiae error, neque at tempora deserunt sint repellendus?",
+//     },
+
+// ]
+
+export const Home = () => {
+
+    const [posts, setPosts] = useState();
+
+    useEffect(() => {
+        fetch("https://my.api.mockaroo.com/posts.json?key=6d1737e0")
+            .then((response) => response.json())
+            .then((posts) => setPosts(posts));
+    }, []);
+
+    console.log("posts from Home component:", posts)
+
+    const updatedPosts = posts?.map((post) => {
+        return ({
+            ...post,
+            postimage: `https://picsum.photos/id/${post.id}/800/400`,
+        });
+    });
+
+    return (
+        <Layout>
+            <div className="main-content right-chat-active">
+                <div className="middle-sidebar-bottom">
+                    <div className="middle-sidebar-left">
+                        <div className="row feed-body">
+                            <div className="col-xl-8 col-xxl-9 col-lg-8">
+                                {updatedPosts &&
+                                    updatedPosts.length > 0 &&
+                                    updatedPosts.map((post) =>
+                                        <PostView {...post} key={post.user} />)}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </Layout>
-);
+        </Layout>
+    );
+};
